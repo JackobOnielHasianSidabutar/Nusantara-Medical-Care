8894824494470332b466d0766eb75ce00e5d335d
JackobOnielHasianSidabutar <jackoboniel1@gmail.com>
2024-12-04 15:25:38 +0700

    menambahkan folder gradle

--- README.md
+++
@@ -1,1 +0,0 @@
-# Nusantara-Medical-Care
---
+++ nmc/src/test/java/com/example/nmc/NmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
